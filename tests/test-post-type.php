db3e83768ea7f8b5b3947664c0025f8a9c262b88
fumikito <guy@hametuha.com>
2018-08-06 14:42:20 +0900

    Add CI test #2

---
+++ tests/test-post-type.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Post type test
+ *
+ * @package Hamelp
+ */
+
+/**
+ * Sample test case.
+ */
+class PostTypeTest extends WP_UnitTestCase {
+
+	/**
+	 * @var \Hametuha\Hamelp\Hooks\PostType
+	 */
+	protected $post_type = null;
+
+	/**
+	 * Override setupper
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->post_type = \Hametuha\Hamelp\Hooks\PostType::get();
+	}
+
+
+	/**
+	 * A single example test.
+	 */
+	public function test_post_types() {
+		$post_types = $this->post_type->get_post_types();
+		$this->assertTrue( is_array( $post_types ) );
+		foreach ( $post_types as $type => $setting ) {
+			$this->assertTrue( post_type_exists( $type ) );
+		}
+	}
+
+	/**
+	 * Check taxonomy
+	 */
+	public function test_taxonomy() {
+		$taxonomy = $this->post_type->taxonomy;
+		$this->assertTrue( taxonomy_exists( $taxonomy ) );
+	}
+}
